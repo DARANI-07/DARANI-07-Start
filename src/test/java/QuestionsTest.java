89d4888dfa5b6372e494b11a4c30f03b9892453a
DARANI-07 <22ad07@drngpit.ac.in>
2026-02-17 23:47:31 +0530

    c

--- src/test/java/QuestionsTest.java
+++ src/test/java/QuestionsTest.java
@@ -2,30 +2,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class QuestionsTest {
+
     Questions q = new Questions();
 
     @Test
     public void testQuestion1() {
-        int expectedResult = (int) Math.PI / 1;
+        int expectedResult = 3; // exact value
         int actualResult = q.question1();
         Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testQuestion2() {
-        int expectedResult = (int) Math.pow(2, 4) - 2;
+        int expectedResult = 14; // exact value
         int actualResult = q.question2();
         Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testQuestion3() {
-        String expectedResult = new StringBuilder("xdeiDgnirtSwLskdu"
-        .substring(5, 11))
-        .reverse()
-        .toString()
-        .toLowerCase();
-        
+        String expectedResult = "string"; // exact string
         String actualResult = q.question3();
         Assert.assertEquals(expectedResult, actualResult.trim().toLowerCase());
     }
