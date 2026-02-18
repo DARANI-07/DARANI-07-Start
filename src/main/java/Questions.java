08941ff68093f674713ceeba094b8e16ce28385e
Daranidaran <22ad007@drngpit.ac.in>
2026-02-18 17:29:14 +0530

    complete

--- src/main/java/Questions.java
+++ src/main/java/Questions.java
@@ -1,14 +1,17 @@
 public class Questions {
 
+    // Q1: How many variables are in Vehicle class? Return exact integer
     public int question1() {
-        return 3;
+        return 3; // currentSpeed, make, model
     }
 
+    // Q2: Number of methods in Vehicle class? Return exact integer
     public int question2() {
-        return 14;
+        return 2; // accelerate() and honk()
     }
 
+    // Q3: What is the type of variable "make" in Vehicle? Return exact string
     public String question3() {
-        return "string";
+        return "String"; // Capital S because portal expects exact type
     }
 }
